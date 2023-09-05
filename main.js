4cda093e849a6ffb7abfcb02202d92d05aac8612
miris18 <92609049+miris18@users.noreply.github.com>
2023-09-05 17:47:04 -0600

    Update main.js

---
+++ main.js
@@ -0,0 +1,19 @@
+      
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+    });
+}
+
+  console.log('ml5 version:', ml5.version);
+
+ function modelLoaded() {
+    console.log('Model Loaded!');
+  }
+      
+  function check()
+  {
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+  }
